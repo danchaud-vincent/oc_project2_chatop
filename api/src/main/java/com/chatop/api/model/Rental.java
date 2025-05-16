31ba0992ceceda3e8bbc63600731eee44e44c6a3
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-16 17:41:03 +0200

    create the rental model

---
+++ api/src/main/java/com/chatop/api/model/Rental.java
@@ -0,0 +1,40 @@
+package com.chatop.api.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "RENTALS")
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private BigDecimal surface;
+    private BigDecimal price;
+    private String picture;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private int ownerId;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+    
+}
