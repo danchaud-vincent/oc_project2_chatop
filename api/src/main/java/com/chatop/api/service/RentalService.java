b71f1b4080e5191cf2c525cb6a6990e73598f85e
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-16 17:54:31 +0200

    create a service folder and a RentalService class

---
+++ api/src/main/java/com/chatop/api/service/RentalService.java
@@ -0,0 +1,21 @@
+package com.chatop.api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chatop.api.model.Rental;
+import com.chatop.api.repository.RentalRepository;
+
+@Service
+public class RentalService {
+
+    @Autowired
+    RentalRepository rentalRepository;
+
+    public List<Rental> getRentals() {
+        return rentalRepository.findAll();
+    }
+
+}
