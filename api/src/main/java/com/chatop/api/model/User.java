b11b206eacbd9d4f48ee08a08831dc39201bdfc5
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-16 17:53:33 +0200

    create a User model

---
+++ api/src/main/java/com/chatop/api/model/User.java
@@ -0,0 +1,29 @@
+package com.chatop.api.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String name;
+    private String password;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+}
