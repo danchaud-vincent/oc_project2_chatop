d9aab9a04485b2aaf79f01f4bdae8265f984db3b
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-16 18:41:56 +0200

    create a UserRepository

---
+++ api/src/main/java/com/chatop/api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.chatop.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.chatop.api.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+
+}
