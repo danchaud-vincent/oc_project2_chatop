f22ed9d28401585869fcedf21552eabc641d0853
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-19 11:42:18 +0200

    add Lombok annotation in rental to deal with constructor

--- api/src/main/java/com/chatop/api/model/Rental.java
+++ api/src/main/java/com/chatop/api/model/Rental.java
@@ -11,10 +11,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "RENTALS")
 public class Rental {
 
