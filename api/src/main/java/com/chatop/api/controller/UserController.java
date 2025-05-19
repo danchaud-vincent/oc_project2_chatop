d943646414d31f7e0cac3999372755bb4e909c88
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-19 18:41:26 +0200

    Create login and register post method in the User Controller

--- api/src/main/java/com/chatop/api/controller/UserController.java
+++ api/src/main/java/com/chatop/api/controller/UserController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chatop.api.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("/auth")
@@ -12,5 +18,21 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody String entity) {
+        
+        return entity;
+    }
+    
+
+    @PostMapping("/register")
+    public String register(@RequestBody String entity) {
+        
+        return entity;
+    }
+    
+    
+    
 
 }
