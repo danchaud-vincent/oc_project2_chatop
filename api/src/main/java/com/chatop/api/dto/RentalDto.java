9028f248fa652a6c43bf395a734c79c6ae4145c5
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-19 12:12:39 +0200

    create a DTO for the rental model

---
+++ api/src/main/java/com/chatop/api/dto/RentalDto.java
@@ -0,0 +1,17 @@
+package com.chatop.api.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class RentalDto {
+
+    private String name;
+    private BigDecimal surface;
+    private BigDecimal price;
+    private String description;
+    private String picture;
+    private int ownerId;
+    private Date createdAt;
+    private Date updatedat;
+
+}
