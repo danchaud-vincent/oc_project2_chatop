0a38db2b2ec1b39fb8ddde747716b0d0ade0bcd3
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-19 18:58:05 +0200

    create the endpoints in UserController

--- api/src/main/java/com/chatop/api/controller/UserController.java
+++ api/src/main/java/com/chatop/api/controller/UserController.java
@@ -1,38 +1,50 @@
 package com.chatop.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chatop.api.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public String login(@RequestBody String entity) {
         
-        return entity;
+        return "Logged in!";
     }
     
 
-    @PostMapping("/register")
+    @PostMapping("/auth/register")
     public String register(@RequestBody String entity) {
         
-        return entity;
+        return "Registered!";
+    }
+
+    @GetMapping("/auth/me")
+    public String getCurrentUser() {
+        return "User";
+    }
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "Get all the users";
+    }
+
+    @GetMapping("/user/{userId}")
+    public String getUser(@PathVariable Integer userId) {
+        return "Get one user";
     }
     
-    
-    
-
 }
