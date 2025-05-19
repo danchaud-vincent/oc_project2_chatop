852314ae431afb5d424b823e5e5f52c4165c9467
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-19 18:30:07 +0200

    create a rental mapper to convert dto to entity

---
+++ api/src/main/java/com/chatop/api/mapper/RentalMapper.java
@@ -0,0 +1,55 @@
+package com.chatop.api.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.chatop.api.dto.RentalCreateDto;
+import com.chatop.api.dto.RentalDto;
+import com.chatop.api.dto.RentalUpdateDto;
+import com.chatop.api.model.Rental;
+import com.chatop.api.model.User;
+
+@Component
+public class RentalMapper {
+
+
+    public RentalDto toDto(Rental rental){
+        RentalDto rentalDto = new RentalDto();
+
+        rentalDto.setId(rental.getId());
+        rentalDto.setName(rental.getName());
+        rentalDto.setSurface(rental.getSurface());
+        rentalDto.setPrice(rental.getPrice());
+        rentalDto.setDescription(rental.getDescription());
+        rentalDto.setPicture(rental.getPicture());
+        rentalDto.setOwnerId(rental.getOwner().getId());
+        rentalDto.setCreatedAt(rental.getCreatedAt());
+        rentalDto.setUpdatedat(rental.getUpdatedAt());
+
+        return rentalDto;
+    }
+
+    public Rental toEntity(RentalCreateDto rentalDto){
+        
+        Rental rental = new Rental();
+
+        rental.setName(rentalDto.getName());
+        rental.setDescription(rentalDto.getDescription());
+        rental.setSurface(rentalDto.getSurface());
+        rental.setPrice(rentalDto.getPrice());
+        rental.setPicture(rentalDto.getPicture());
+        
+        return rental;
+    }
+
+
+    public Rental updateEntity(Rental rental, RentalUpdateDto rentalUpdatedDto){
+
+        rental.setName(rentalUpdatedDto.getName());
+        rental.setSurface(rentalUpdatedDto.getSurface()); 
+        rental.setPrice(rentalUpdatedDto.getPrice());
+        rental.setDescription(rentalUpdatedDto.getDescription());
+
+        return rental;
+    }
+
+}
