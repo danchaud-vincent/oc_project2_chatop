d4eb5c5bdcaa08b85ff7125ec3f06193624f8131
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-19 16:27:46 +0200

    create a globalExceptionHandler with @ControllerAdvice to handle exceptions at a central place

---
+++ api/src/main/java/com/chatop/api/controller/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.chatop.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleNotFoundException(RuntimeException exception){
+        return new ResponseEntity<String>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+
+}
