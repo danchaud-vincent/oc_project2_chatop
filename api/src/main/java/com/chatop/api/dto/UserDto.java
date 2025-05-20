55427035ad8d62a82858f07fcec3f370639b0797
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-20 12:30:41 +0200

    Create a UserDto

---
+++ api/src/main/java/com/chatop/api/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.chatop.api.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private Date createdAt;
+    private Date updatedAt;
+    
+
+}
