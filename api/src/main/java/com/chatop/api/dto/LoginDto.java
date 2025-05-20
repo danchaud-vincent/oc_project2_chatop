835e0dce254f4c8aa8790ed8f5844cd8f18ad1f2
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-20 12:32:14 +0200

    create a Login Dto

---
+++ api/src/main/java/com/chatop/api/dto/LoginDto.java
@@ -0,0 +1,16 @@
+package com.chatop.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+
+    private String email;
+    private String name;
+    private String password;
+
+}
