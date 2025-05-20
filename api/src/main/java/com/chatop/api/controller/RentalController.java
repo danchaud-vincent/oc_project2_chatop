6c0c7f263517c44fac372309b12e420f5eb86e70
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-20 12:10:58 +0200

    add @CrossOrigin to the Rental controller

--- api/src/main/java/com/chatop/api/controller/RentalController.java
+++ api/src/main/java/com/chatop/api/controller/RentalController.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping("/api")
 public class RentalController {
 
