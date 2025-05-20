d31911acdee7e0a50ba7d19851424530bc55ba31
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-20 17:50:43 +0200

    create a register method in the user service to save a new user when registered

--- api/src/main/java/com/chatop/api/service/UserService.java
+++ api/src/main/java/com/chatop/api/service/UserService.java
@@ -1,7 +1,10 @@
 package com.chatop.api.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.chatop.api.dto.RegisterDto;
+import com.chatop.api.model.User;
 import com.chatop.api.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,6 +13,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public String register(RegisterDto registerDto){
+
+        User newUser = new User();
+        newUser.setEmail(registerDto.getEmail());
+        newUser.setName(registerDto.getName());
+        newUser.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        userRepository.save(newUser);
+
+        return "register";
+    }
 
 }
