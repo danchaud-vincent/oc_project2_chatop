afff1253193add425d06e67853aca6c22ac4b9c6
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-23 17:10:36 +0200

    Create a UserMapper to convert user entity to dto

---
+++ api/src/main/java/com/chatop/api/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.chatop.api.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.chatop.api.dto.UserDto;
+import com.chatop.api.model.User;
+
+@Component
+public class UserMapper {
+
+
+    public UserDto toDto(User user){
+
+        UserDto userDto = new UserDto();
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        userDto.setCreatedAt(user.getCreatedAt());
+        userDto.setUpdatedAt(user.getUpdatedAt());
+
+        return userDto;
+
+    }
+
+}
