5ea82575377de68c708b0376df04b940a627d8d8
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-23 20:24:03 +0200

    Create a response dto when we create a new rental

---
+++ api/src/main/java/com/chatop/api/dto/ResponseRentalDto.java
@@ -0,0 +1,14 @@
+package com.chatop.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseRentalDto {
+
+    private String message;
+
+}
