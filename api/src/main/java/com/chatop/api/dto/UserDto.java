baecdb86ca1c1d5863d993256d697b7531b3b8c6
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-23 18:03:09 +0200

    add id in the User dto, and modify user mapper

--- api/src/main/java/com/chatop/api/dto/UserDto.java
+++ api/src/main/java/com/chatop/api/dto/UserDto.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserDto {
 
+    private Integer id;
     private String name;
     private String email;
     private Date createdAt;
--- api/src/main/java/com/chatop/api/mapper/UserMapper.java
+++ api/src/main/java/com/chatop/api/mapper/UserMapper.java
@@ -12,6 +12,7 @@ public class UserMapper {
     public UserDto toDto(User user){
 
         UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
         userDto.setName(user.getName());
         userDto.setEmail(user.getEmail());
         userDto.setCreatedAt(user.getCreatedAt());
