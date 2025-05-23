08c04c62199c7b216bb100d800321b844954dbe3
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-23 11:50:05 +0200

    Add the Optional var in the UserRepository for the findByEmail method

--- api/src/main/java/com/chatop/api/repository/UserRepository.java
+++ api/src/main/java/com/chatop/api/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.chatop.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.chatop.api.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
