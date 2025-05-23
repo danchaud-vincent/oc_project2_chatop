8bb49ad17b60fd99b11f77e1aef0619495daf6bc
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-23 16:04:36 +0200

    create the generateToken method in JWTservice using the JwtEncoder

--- api/src/main/java/com/chatop/api/service/JWTService.java
+++ api/src/main/java/com/chatop/api/service/JWTService.java
@@ -1,9 +1,43 @@
 package com.chatop.api.service;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 
 @Service
+@RequiredArgsConstructor
 public class JWTService {
+
+    private static final Integer MINUTES = 60;
+    private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+
+    public String generateToken(String email) {
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+            .issuer("self")
+            .issuedAt(now)
+            .expiresAt(now.plus(MINUTES, ChronoUnit.MINUTES))
+            .subject(email)
+            .build();
+
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(JwsHeader.with(
+            MacAlgorithm.HS256).build(), 
+            claims
+        );
+        
+        return jwtEncoder.encode(jwtEncoderParameters).getTokenValue();    
+    }
 
 
 }
