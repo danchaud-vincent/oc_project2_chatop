e4429d4821f90fef6a9af8f8684b775e5ad0fdbc
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-26 16:44:52 +0200

    create a response dto folder, create a ResponseRentalsDTO and put all the response dtos inside the new folder

---
+++ api/src/main/java/com/chatop/api/dto/response/ResponseRentalsDto.java
@@ -0,0 +1,18 @@
+package com.chatop.api.dto.response;
+
+import java.util.List;
+
+import com.chatop.api.dto.RentalDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseRentalsDto {
+
+    public List<RentalDto> rentals;
+
+}
