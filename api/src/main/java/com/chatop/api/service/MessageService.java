8986edb0f6669b18e1d7617a1cc660ab6b612646
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-26 19:54:03 +0200

    create a message service and a sendMessage method

---
+++ api/src/main/java/com/chatop/api/service/MessageService.java
@@ -0,0 +1,22 @@
+package com.chatop.api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.chatop.api.repository.MessageRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public void getMessages(){
+
+    }
+
+    public String sendMessage(String message){
+        return "Message";
+    }
+}
