acba0e9ae2b962257f3e8b12696f83ae7cef18ae
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-27 19:02:19 +0200

    add swagger doc and schema in user dto

--- api/src/main/java/com/chatop/api/dto/UserDto.java
+++ api/src/main/java/com/chatop/api/dto/UserDto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,15 +12,23 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Objet représentant un Utilisateur")
 public class UserDto {
 
+    @Schema(description = "Identifiant unique de l'utilisateur")
     private Integer id;
+   
+    @Schema(description = "Nom complet de l'utilisateur")
     private String name;
+
+    @Schema(description = "email de l'utilisateur")
     private String email;
 
+    @Schema(description = "date de création de l'utilisateur")
     @JsonProperty("created_at")
     private Date createdAt;
 
+    @Schema(description = "date d'une mise à jour d'information")
     @JsonProperty("updated_at")
     private Date updatedAt;
 
