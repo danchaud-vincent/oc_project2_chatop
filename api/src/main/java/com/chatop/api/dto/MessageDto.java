6f0cd91981ae5a1e88e0574efabb38c3b1cabf0a
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-27 15:28:02 +0200

    add jsonproperty to the dto in order to retrieve the values coming from the frontend

--- api/src/main/java/com/chatop/api/dto/MessageDto.java
+++ api/src/main/java/com/chatop/api/dto/MessageDto.java
@@ -1,5 +1,7 @@
 package com.chatop.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MessageDto {
 
+    @JsonProperty("user_id")
     private Integer userId;
+
+    @JsonProperty("rental_id")
     private Integer rentalId;
+
     private String message;
 
 }
