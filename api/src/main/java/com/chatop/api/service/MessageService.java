8349ace76e48c6d63e5c7dde53dd48e400ba6336
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-27 15:10:54 +0200

    create and update the sendMessage method in message service by adding the dto and mapper

--- api/src/main/java/com/chatop/api/service/MessageService.java
+++ api/src/main/java/com/chatop/api/service/MessageService.java
@@ -2,6 +2,10 @@ package com.chatop.api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.chatop.api.dto.MessageDto;
+import com.chatop.api.dto.response.ResponseMessageDto;
+import com.chatop.api.mapper.MessageMapper;
+import com.chatop.api.model.Message;
 import com.chatop.api.repository.MessageRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,14 +15,17 @@ import lombok.RequiredArgsConstructor;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final MessageMapper messageMapper;
 
     public void getMessages(){
 
     }
 
-    public String sendMessage(String message){
-        
+    public ResponseMessageDto sendMessage(MessageDto messageDto){
 
-        return "Message";
+        Message messageEntity = messageMapper.toEntity(messageDto);
+        messageRepository.save(messageEntity);
+        
+        return new ResponseMessageDto(messageDto.getMessage());
     }
 }
