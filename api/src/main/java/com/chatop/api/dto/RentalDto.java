80104c86fb7fe9ea9df5e64f9934c29912390587
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-27 19:07:12 +0200

    add swagger schema and doc in rental dto

--- api/src/main/java/com/chatop/api/dto/RentalDto.java
+++ api/src/main/java/com/chatop/api/dto/RentalDto.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,21 +13,36 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Objet représentant une location")
 public class RentalDto {
 
+    @Schema(description = "Identifiant unique de la location")
     private Integer id;
+
+    @Schema(description = "Nom de la location")
     private String name;
+
+    @Schema(description = "Surface en mètres carrés de la location")
     private BigDecimal surface;
+
+    @Schema(description = "Prix de la location")
     private BigDecimal price;
+
+    @Schema(description = "Description supplémentaire sur la location")
     private String description;
+
+    @Schema(description = "image représentant la location")
     private String picture;
 
+    @Schema(description = "Identifiant unique de l'utilisateur")
     @JsonProperty("owner_id")
     private Integer ownerId;
 
+    @Schema(description = "Date de création de l'annonce")
     @JsonProperty("created_at")
     private Date createdAt;
 
+    @Schema(description = "Date de mise à jour d'une information sur la location")
     @JsonProperty("updated_at")
     private Date updatedat;
 
