e00a9c7155516fe993fb044b1488b875813fe15d
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-27 18:53:27 +0200

    add swagger doc in the messages controller

--- api/src/main/java/com/chatop/api/controller/MessagesController.java
+++ api/src/main/java/com/chatop/api/controller/MessagesController.java
@@ -11,6 +11,9 @@ import com.chatop.api.dto.response.ResponseMessageDto;
 import com.chatop.api.dto.response.ResponseMessagesDto;
 import com.chatop.api.service.MessageService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,16 +25,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
+@Tag(name = "Messages", description = "Opérations sur les messages")
 @RequestMapping("/api")
 public class MessagesController {
     
     private final MessageService messageService;
 
+    @Operation(
+        summary = "Récupère l'ensemble des messages",
+        description = "Permet de récupèrer la liste des messages",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Messages trouvés")
+        }
+    )
     @GetMapping("/messages")
     public ResponseEntity<ResponseMessagesDto> getMessages(){
         return new ResponseEntity<ResponseMessagesDto>(messageService.getMessages(), HttpStatus.OK);
     }
 
+    @Operation(
+        summary = "Envoi un nouveau message",
+        description = "Permet d'envoyer un message à un utilisateur",
+        responses = {
+            @ApiResponse(responseCode = "201", description = "Message envoyé")
+        }
+    )
     @PostMapping("/messages")
     public ResponseEntity<ResponseMessageDto> sendMessage(
         @RequestBody MessageDto messageRequest) {
