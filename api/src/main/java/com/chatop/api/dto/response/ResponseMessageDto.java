9f3bc059c83ccafdd1eaf26789a135df2caea9a1
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-27 15:09:38 +0200

    create a message response dto

---
+++ api/src/main/java/com/chatop/api/dto/response/ResponseMessageDto.java
@@ -0,0 +1,14 @@
+package com.chatop.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseMessageDto {
+
+    private String message;
+
+}
