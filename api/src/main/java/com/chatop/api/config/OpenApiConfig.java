2b58e6c729bb2e9e2003043af61bccb06acbd682
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-28 12:58:16 +0200

    create a OpenApiConfig to handle the token authorization in the api doc

---
+++ api/src/main/java/com/chatop/api/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.chatop.api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+@SecurityRequirement(name = "bearerAuth")
+public class OpenApiConfig {
+
+}
