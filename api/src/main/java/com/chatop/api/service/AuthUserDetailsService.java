50d0045b4b17896da670bc5383c7d178584f7c48
Vincent Danchaud <vdanchaud@gmail.com>
2025-05-28 16:38:54 +0200

    modify the AuthUSerDetailsService by adding the UserNotFoundException

--- api/src/main/java/com/chatop/api/service/AuthUserDetailsService.java
+++ api/src/main/java/com/chatop/api/service/AuthUserDetailsService.java
@@ -2,9 +2,9 @@ package com.chatop.api.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.chatop.api.exception.UserNotFoundException;
 import com.chatop.api.model.User;
 import com.chatop.api.model.auth.AuthUser;
 import com.chatop.api.repository.UserRepository;
@@ -18,10 +18,10 @@ public class AuthUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UserNotFoundException {
 
         User user = userRepository.findByEmail(email).orElseThrow(() ->
-            new UsernameNotFoundException(String.format("No user found with the email '%s'", email))
+            new UserNotFoundException(String.format("No user found with the email '%s'", email))
         );
         
         return new AuthUser(user);
