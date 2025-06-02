48c906df2a05eb0ac417b543ae0b1864f48ba20b
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-02 16:32:59 +0200

    clean the code

--- api/src/main/java/com/chatop/api/service/UserService.java
+++ api/src/main/java/com/chatop/api/service/UserService.java
@@ -4,10 +4,8 @@ package com.chatop.api.service;
 import java.util.Optional;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
