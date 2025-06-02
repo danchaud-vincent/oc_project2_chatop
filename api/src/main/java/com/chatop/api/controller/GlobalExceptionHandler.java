f0bdc794e777005c2d78a6f649f8409dfc39552a
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-02 16:31:42 +0200

    add the Error handler for the bad requests and server error in the Global Exception handler

--- api/src/main/java/com/chatop/api/controller/GlobalExceptionHandler.java
+++ api/src/main/java/com/chatop/api/controller/GlobalExceptionHandler.java
@@ -2,15 +2,44 @@ package com.chatop.api.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.chatop.api.exception.InvalidCredentialsException;
 import com.chatop.api.exception.UserAlreadyExistsException;
 import com.chatop.api.exception.UserNotFoundException;
+import com.chatop.api.model.ErrorResponse;
+
+import jakarta.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Handle 400 - Bad Request
+    @ExceptionHandler({
+        MethodArgumentNotValidException.class,
+        ConstraintViolationException.class,
+        MethodArgumentTypeMismatchException.class,
+        MissingServletRequestParameterException.class,
+        HttpMessageNotReadableException.class
+    })
+    public ResponseEntity<ErrorResponse> handleBadRequest(Exception ex){
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), String.format("Bad Request: %s", ex.getMessage()));
+        
+        return new ResponseEntity<ErrorResponse>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    // handle 500 - Internal server error
+
+    public ResponseEntity<ErrorResponse> handleInternalError(Exception ex){
+        ErrorResponse error = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), String.format("Internal server error: %s", ex.getMessage()));
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     // Exceptions handler for users
     @ExceptionHandler(UserNotFoundException.class)
