5986ca0110b8a52661d211eae2572d09415fbff0
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-03 16:45:05 +0200

    clean code in rental mapper

--- api/src/main/java/com/chatop/api/mapper/RentalMapper.java
+++ api/src/main/java/com/chatop/api/mapper/RentalMapper.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.chatop.api.dto.RentalCreateDto;
 import com.chatop.api.dto.RentalDto;
