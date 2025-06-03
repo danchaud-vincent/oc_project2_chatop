09dc6ef901d6099f6ff64f8448cff53dbf24f1c2
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-03 17:06:13 +0200

    create dto folder message, add MessageDto and renamed it MessageRequestDto

---
+++ api/src/main/java/com/chatop/api/dto/message/MessageRequestDto.java
@@ -0,0 +1,26 @@
+package com.chatop.api.dto.message;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Objet représentant un Message")
+public class MessageRequestDto {
+
+    @Schema(description = "Identifiant de l'utilisateur")
+    @JsonProperty("user_id")
+    private Integer userId;
+
+    @Schema(description = "Identifiant de la location")
+    @JsonProperty("rental_id")
+    private Integer rentalId;
+
+    private String message;
+
+}
