cd22e68568dbefcf14af166b95ce7f60a9bf4956
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-03 16:33:23 +0200

    update the security config by modifying the image path

--- api/src/main/java/com/chatop/api/config/SecurityConfig.java
+++ api/src/main/java/com/chatop/api/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         return http.csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
                     .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
-                    .requestMatchers("/api/uploads/images/**").permitAll()
+                    .requestMatchers("/api/rentals/images/**").permitAll()
                     .requestMatchers("/v3/api-docs/**").permitAll()
                     .requestMatchers("/swagger-ui/**").permitAll()
                     .anyRequest().authenticated())
