995cc022a771e026b7391fd71bd3e9858d310c4f
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-03 16:39:03 +0200

    update the image controller

--- api/src/main/java/com/chatop/api/controller/ImageController.java
+++ api/src/main/java/com/chatop/api/controller/ImageController.java
@@ -1,91 +1,91 @@
-package com.chatop.api.controller;
+// package com.chatop.api.controller;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.nio.file.Paths;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.core.io.Resource;
+// import org.springframework.core.io.UrlResource;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.MediaType;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.chatop.api.model.ErrorResponse;
+// import com.chatop.api.model.ErrorResponse;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
+// import io.swagger.v3.oas.annotations.Operation;
+// import io.swagger.v3.oas.annotations.media.Content;
+// import io.swagger.v3.oas.annotations.media.Schema;
+// import io.swagger.v3.oas.annotations.responses.ApiResponse;
+// import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+// import io.swagger.v3.oas.annotations.tags.Tag;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
-
-@SecurityRequirement(name = "bearerAuth")
-@RestController
-@CrossOrigin
-@Tag(name = "Images", description = "Methods on images")
-@RequestMapping("/api/uploads")
-public class ImageController {
-
-    @Value("${file.upload-dir}")
-    private String uploadDir;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
 
 
-    @Operation(
-        summary = "Get the image of a rental",
-        description = "Retrieve the image of a rental with its filepath",
-        responses = {
-            @ApiResponse(responseCode = "200", description = "Image found"),
-            @ApiResponse(
-                responseCode = "400", 
-                description = "Bad request", 
-                content = @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = ErrorResponse.class)
-                )),
-            @ApiResponse(
-                responseCode = "500",
-                description = "Internal server error",
-                content = @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = ErrorResponse.class)
-                )
-            )
-        }
-    )
-    @GetMapping("/images/{filename}")
-    public ResponseEntity<Resource> getImage(@PathVariable String filename)  {
 
-        try {
-            Path filePath = Paths.get(uploadDir).resolve(filename).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
+// @SecurityRequirement(name = "bearerAuth")
+// @RestController
+// @CrossOrigin
+// @Tag(name = "Images", description = "Methods on images")
+// @RequestMapping("/api/uploads")
+// public class ImageController {
 
-            if (resource.exists()){
-                String contentType = Files.probeContentType(filePath);
+//     @Value("${file.upload-dir}")
+//     private String uploadDir;
 
-                return ResponseEntity.ok()
-                    .contentType(MediaType.parseMediaType(contentType))
-                    .body(resource);
-            }
-            else{
-                return ResponseEntity.notFound().build();
-            }
+
+//     @Operation(
+//         summary = "Get the image of a rental",
+//         description = "Retrieve the image of a rental with its filepath",
+//         responses = {
+//             @ApiResponse(responseCode = "200", description = "Image found"),
+//             @ApiResponse(
+//                 responseCode = "400", 
+//                 description = "Bad request", 
+//                 content = @Content(
+//                     mediaType = "application/json",
+//                     schema = @Schema(implementation = ErrorResponse.class)
+//                 )),
+//             @ApiResponse(
+//                 responseCode = "500",
+//                 description = "Internal server error",
+//                 content = @Content(
+//                     mediaType = "application/json",
+//                     schema = @Schema(implementation = ErrorResponse.class)
+//                 )
+//             )
+//         }
+//     )
+//     @GetMapping("/images/{filename}")
+//     public ResponseEntity<Resource> getImage(@PathVariable String filename)  {
+
+//         try {
+//             Path filePath = Paths.get(uploadDir).resolve(filename).normalize();
+//             Resource resource = new UrlResource(filePath.toUri());
+
+//             if (resource.exists()){
+//                 String contentType = Files.probeContentType(filePath);
+
+//                 return ResponseEntity.ok()
+//                     .contentType(MediaType.parseMediaType(contentType))
+//                     .body(resource);
+//             }
+//             else{
+//                 return ResponseEntity.notFound().build();
+//             }
             
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+//         } catch (Exception e) {
+//             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//         }
         
-    }
+//     }
     
 
 
 
-}
+// }
