f3b7b64e572869312d075becce39664f0a788e49
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-04 17:00:36 +0200

    create a toEntity method in Usermapper

--- api/src/main/java/com/chatop/api/mapper/UserMapper.java
+++ api/src/main/java/com/chatop/api/mapper/UserMapper.java
@@ -1,12 +1,19 @@
 package com.chatop.api.mapper;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import com.chatop.api.dto.auth.RegisterRequestDto;
 import com.chatop.api.dto.auth.UserResponseDto;
 import com.chatop.api.model.User;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class UserMapper {
 
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponseDto toDto(User user){
 
@@ -19,6 +26,15 @@ public class UserMapper {
 
         return userDto;
 
+    }
+
+    public User toEntity(RegisterRequestDto userRegistered){
+        User user = new User();
+        user.setEmail(userRegistered.getEmail());
+        user.setName(userRegistered.getName());
+        user.setPassword(passwordEncoder.encode(userRegistered.getPassword()));
+
+        return user;
     }
 
 }
