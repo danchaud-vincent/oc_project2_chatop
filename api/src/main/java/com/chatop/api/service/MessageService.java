827dd52c0656b57c4f6b1330c99f7252470611c6
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-04 16:43:39 +0200

    add error handlers by checking if the user exists or the rental exists in the db

--- api/src/main/java/com/chatop/api/service/MessageService.java
+++ api/src/main/java/com/chatop/api/service/MessageService.java
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Service;
 
 import com.chatop.api.dto.message.MessageRequestDto;
 import com.chatop.api.dto.message.MessageResponseDto;
+import com.chatop.api.exception.RentalNotFoundException;
+import com.chatop.api.exception.UserNotFoundException;
 import com.chatop.api.mapper.MessageMapper;
 import com.chatop.api.model.Message;
 import com.chatop.api.repository.MessageRepository;
+import com.chatop.api.repository.RentalRepository;
+import com.chatop.api.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,8 +20,20 @@ public class MessageService {
 
     private final MessageRepository messageRepository;
     private final MessageMapper messageMapper;
+    private final UserRepository userRepository;
+    private final RentalRepository rentalRepository;
 
     public MessageResponseDto sendMessage(MessageRequestDto messageDto){
+
+        // check if the user exist
+        userRepository.findById(messageDto.getUserId()).orElseThrow(() ->
+            new UserNotFoundException("User not found with id : " + messageDto.getUserId())
+        );
+
+        // check if the rental exist
+        rentalRepository.findById(messageDto.getRentalId()).orElseThrow(() ->
+            new RentalNotFoundException("Rental not found with id : " + messageDto.getRentalId())
+        );
 
         Message messageEntity = messageMapper.toEntity(messageDto);
         messageRepository.save(messageEntity);
