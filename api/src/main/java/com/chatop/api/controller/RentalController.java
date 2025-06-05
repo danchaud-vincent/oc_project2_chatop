dbc719a0c4acaaddad8ee10ff03de4da11845edd
Vincent Danchaud <vdanchaud@gmail.com>
2025-06-05 16:27:50 +0200

    clean code

--- api/src/main/java/com/chatop/api/controller/RentalController.java
+++ api/src/main/java/com/chatop/api/controller/RentalController.java
@@ -175,11 +175,9 @@ public class RentalController {
         Authentication authentication) throws IOException{
         
         double maxSize = 2e6;
-        System.out.println(imageFile.getSize());
-
+        
         // check the size of the image
         if (imageFile.getSize() > maxSize){
-            System.out.println("IMAGE TOO BIG");
             return new ResponseEntity<RentalCreateResponseDto>(new RentalCreateResponseDto("Image Size too big"), HttpStatus.BAD_REQUEST);
         }
         
